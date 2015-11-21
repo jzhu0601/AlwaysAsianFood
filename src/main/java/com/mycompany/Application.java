f6614a1850d6029983d4c4d47af674715684743f
jzhu0601 <accazk1@gmail.com>
2015-11-21 11:33:16 -0500

    nothing

--- src/main/java/com/mycompany/Application.java
+++ src/main/java/com/mycompany/Application.java
@@ -2,6 +2,7 @@ package com.mycompany;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Created by jzhu on 11/20/2015.
