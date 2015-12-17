316711687c0c386701df69fecca6f518aea94216
jzhu0601 <accazk1@gmail.com>
2015-12-17 09:05:15 -0500

    Merged commits from testing branch to master

--- src/main/java/com/mycompany/controllers/CustomerController.java
+++ src/main/java/com/mycompany/controllers/CustomerController.java
@@ -1,8 +1,6 @@
 package com.mycompany.controllers;
 
 import com.mycompany.entities.Customer;
-import com.mycompany.entities.Restaurant;
-import com.mycompany.services.CustomerService;
 import com.mycompany.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -26,7 +24,7 @@ public class CustomerController {
 
         Collection<Customer> customers = customerService.findAll();
 
-        return new ResponseEntity<Collection<Customer>>(customers, HttpStatus.OK);
+        return new ResponseEntity<>(customers, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/api/customers/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -34,13 +32,13 @@ public class CustomerController {
 
         Customer customer = customerService.findOne(customerId);
 
-        return new ResponseEntity<Customer>(customer, HttpStatus.OK);
+        return new ResponseEntity<>(customer, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/api/customers", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Customer> createCustomer(@RequestBody Customer customer) {
         Customer customer1 = customerService.create(customer);
-        return new ResponseEntity<Customer>(customer1, HttpStatus.CREATED);
+        return new ResponseEntity<>(customer1, HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/api/customers/{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -49,15 +47,15 @@ public class CustomerController {
 
         Customer customer1 = customerService.update(customer);
         if (customer1 == null) {
-            return new ResponseEntity<Customer>(HttpStatus.INTERNAL_SERVER_ERROR);
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        return new ResponseEntity<Customer>(customer1, HttpStatus.OK);
+        return new ResponseEntity<>(customer1, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/api/customers/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<Customer> deleteCustomer(@PathVariable("id") Integer id) {
         customerService.delete(id);
-        return new ResponseEntity<Customer>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 
